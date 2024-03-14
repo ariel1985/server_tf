ef7d4b303e9baaf100edfe6842637b0e7e3e74fc
ariel1985 <rothemariel@gmail.com>
2024-03-14 21:38:30 +0200

    update model

--- Models/TranslationData.cs
+++ Models/TranslationData.cs
@@ -1,6 +1,5 @@
 public class TranslationData
 {
-    public string? BasicPath { get; set; }
     public DateTime LastUpdate { get; set; }
     public Dictionary<string, Dictionary<string, string>>? Translations { get; set; }
 }
